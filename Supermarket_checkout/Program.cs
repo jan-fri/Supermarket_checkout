4ea8ab29185e1f388d6f3facdfd6f9aef5166261
unknown <janfrindt@hotmail.com>
2019-11-05 18:53:30 +0000

    create project

---
+++ Supermarket_checkout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Supermarket_checkout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
